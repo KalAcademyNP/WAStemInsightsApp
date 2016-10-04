8fa68cfbf1eecba0e952c4b58eb44cb5984dceb8
GS <gayathri.shivaraj@gmail.com>
2016-10-03 18:23:21 -0700

    WASTEMInsight

---
+++ STEMInsightsAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="STEMInsightsAPI.WebApiApplication" Language="C#" %>
---
+++ STEMInsightsApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace STEMInsightsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult StemInsights()
+        {
+            ViewBag.Message = "Stem Insights page.";
+
+            return View();
+        }
+    }
+}
